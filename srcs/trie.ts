a12fa764d0e1b47db80eaa7af6ba6e39c85758f3
Concepty <khs1903b@gmail.com>
2024-06-23 13:12:27 +0900

    update & refactor: update addChild logic
    fix: edge case bug - empty string
    update & refactor: add Child logic
    add: addMaterials (array of material) and related updates for metadata.

--- srcs/trie.ts
+++ srcs/trie.ts
@@ -5,17 +5,31 @@ export class Trie {
         this.roots = roots?? new Map<string, Node>();
     }
 
+    /**
+     * Add a new Node to this Trie. This method does nothing for empty `fullstr`.
+     * If `material` is provided, it will be added to the target node.
+     * @param fullstr string to add. Provide non-empty `fullstr`
+     * @param material NodeMaterial for new Node
+     * @returns 
+     */
     addNode(fullstr: string, material?: NodeMaterial) {
+        if (fullstr.length == 0) return;
         const firstChar = fullstr.charAt(0);
         let root = this.roots.get(firstChar);
         if (!root) {
             root = new Node();
             this.roots.set(firstChar, root);
         }
+        if (fullstr.length == 1) return;
 
-        root.addNode(fullstr, material);
+        root.addNode(fullstr.slice(1), material);
     }
 
+    /**
+     * Find a Node by `query`. It returns null for empty `query`
+     * @param query query string for Node.
+     * @returns Found Node or null
+     */
     findNode(query: string): Node | null {
         let nodeSet = this.roots;
 
@@ -27,6 +41,14 @@ export class Trie {
 
         return nodeSet.get(query.slice(-1)) ?? null;
     }
+
+    suggestNodeMaterials(query: string): NodeMaterial[] {
+        if (query.length == 0) return [];
+
+        const queryNode = this.findNode(query);
+
+        return [];
+    }
 }
 
 export class Node {
@@ -35,7 +57,7 @@ export class Node {
     metadata: NodeMetadata; // not-null
     materials: NodeMaterial[];
 
-    constructor(materials?: NodeMaterial[] | null, parent: Node | null = null) {
+    constructor(parent: Node | null = null, materials?: NodeMaterial[] | null) {
         this.parent = parent;
         this.children = new Map<string, Node>();
 
@@ -44,34 +66,39 @@ export class Node {
         this.materials = materials ?? [];
     }
 
-    addChild(key: string, child: Node) {
+    /**
+     * This method replaces addChild.
+     * Find and return child with given `key`, and if there is no such child, add child.
+     * @param key sould be length == 1 string.
+     * @returns child with given key
+     */
+    getChildOfKey(key: string): Node | null {
+        if(key.length !== 1) return null;
+
+        const node = this.children.get(key);
+        if (node) return node;
+
+        const child = new Node(this);
         this.children.set(key, child);
-        child.parent = this;
+        return child;
     }
 
     /**
-     * Add nood considering current node as root for @param fullstr. 
-     * @param root 
-     * @param fullstr full string includig this node's key
-     * @param material 
+     * Add a Node with `fullstr` assuming this Node as a root.
+     * @param fullstr string of node to add excludes the first char for current node. 
+     * It ignores empty string.
+     * @param material If exists, it is added to the target node.
      */
     addNode(fullstr: string, material?: NodeMaterial) {
-        let newNode = new Node();
-        let cursor: Node = this;
-        for (const key of fullstr.slice(1, -1)) {
-            cursor = cursor.getChildOfKey(key);
-        }
-        cursor.addChild(fullstr.slice(-1), newNode);
-        if (material) newNode.addMaterial(material, true);
-    }
+        if (fullstr.length == 0) return;
 
-    getChildOfKey(key: string): Node {
-        let child = this.children.get(key);
-        if (!child) {
-            child = new Node(null, this);
-            this.children.set(key, child);
-        }        
-        return child;
+        let cursor: Node = this;
+
+        for (const key of fullstr) {
+            cursor = cursor.getChildOfKey(key)!;
+        }
+        
+        if (material) cursor.addMaterial(material, true);
     }
 
     addMaterial(material: NodeMaterial, updateAncestorMetadata: boolean) {
@@ -82,15 +109,36 @@ export class Node {
             this.parent.updateParentMetadataUptoRoot(material);
     }
 
-    updateParentMetadataUptoRoot(material: NodeMaterial) {
+    addMaterials(materials: NodeMaterial[], updateAncestorMetadata: boolean) {
+        this.materials.push(...materials);
+        materials.forEach(m => m.moveNode(this));
+
+        if (updateAncestorMetadata && this.parent) {
+            this.parent.updateParentMetadataUptoRoot(materials);
+        }
+    }
+
+    // refactor
+    updateParentMetadataUptoRoot(changes: NodeMaterial | NodeMaterial[]) {
         var cursor: typeof this.parent = this;
-        while (cursor?.metadata.updatePromisingMaterial(material)) {
-            cursor = cursor.parent;
+        
+        if (changes instanceof NodeMaterial) {
+            while (cursor?.metadata.updatePromisingMaterial(changes)) {
+                cursor = cursor.parent;
+            }
+        } else if (Array.isArray(changes)){
+            while (cursor?.metadata.updatePromisingMaterials(changes)) {
+                cursor = cursor.parent;
+            }
         }
     }
 
     getMaterial(content: any): NodeMaterial | null {
         return this.materials.find(m => m.hasContent(content)) ?? null;
+    }
+
+    getMaterials(): NodeMaterial[] {
+        return this.materials;
     }
 }
 
@@ -103,19 +151,19 @@ export class NodeMetadata {
         this.promisingMaterials = [];
     }
 
+    // refactor for efficiency
     updatePromisingMaterial(material: NodeMaterial): boolean {
-        
-        if (this.promisingMaterials.some(m => m.equal(material))) {
-            this.promisingMaterials.sort((a, b) => this.calculateImportance(b) - this.calculateImportance(a));
-            return true;
-        }
-        
         if (this.promisingMaterials.length == 0) {
             this.promisingMaterials.push(material);
             return true;
         }
+        
+        if (this.promisingMaterials.some(m => m.equal(material))) {
+            this.promisingMaterials.sort((a, b) => this.getSortScore(b) - this.getSortScore(a));
+            return true;
+        }
 
-        const index = this.promisingMaterials.findIndex(m => this.calculateImportance(m) <= this.calculateImportance(material));
+        const index = this.promisingMaterials.findIndex(m => this.getSortScore(m) <= this.getSortScore(material));
 
         if (index == -1) return false;
 
@@ -125,7 +173,19 @@ export class NodeMetadata {
         return true;
     }
 
-    calculateImportance(material: NodeMaterial): number {
+    // refactor for efficiency
+    updatePromisingMaterials(materials: NodeMaterial[]): boolean {
+        materials.forEach(nm => {
+            if (!this.promisingMaterials.some(em => em.equal(nm)))
+                this.promisingMaterials.push(nm);
+        })
+        this.promisingMaterials.sort((a, b) => this.getSortScore(b) - this.getSortScore(a));
+
+        // not implemented - this method currently doesn't check if metadata is updated or not.
+        return true;
+    }
+
+    getSortScore(material: NodeMaterial): number {
         return material.useCount;
     }
 }
