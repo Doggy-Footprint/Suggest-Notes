ac271899b57158cd7a86a56496b15f7fdadfcfff
Concepty <khs1903b@gmail.com>
2024-06-23 12:52:49 +0900

    test: tests for Trie
    duplicate test sets are in red state.

--- srcs/__tests__/test_trie.ts
+++ srcs/__tests__/test_trie.ts
@@ -1,28 +1,47 @@
 import { Trie, Node, NodeMetadata, NodeMaterial } from '../trie';
 
-function getDefaultTrie(): Trie {
-    const trie = new Trie();
-    trie.addNode("cgroup", new NodeMaterial("PKM/contents/cgroup.md"));
-    trie.addNode("closure", new NodeMaterial("PKM/contents/Closure.md"));
-    trie.addNode("docker", new NodeMaterial("PKM/contents/Docker.md"));
-    trie.addNode("database", new NodeMaterial("PKM/contents/Database.md"));
-    trie.addNode("docker cli", new NodeMaterial("PKM/contents/Docker CLI.md"));
-    trie.addNode("dockerfile", new NodeMaterial("PKM/contents/Dockerfile.md"));
+describe('Trie structure ', () => {
+    let trie: Trie;
 
-    /**
-     * intended usage is suggesting possible materials, not user gives content nor materials. it's for testing
-     */
-    const docker = trie.findNode("docker");
-    const docker_content = docker?.getMaterial("PKM/contents/Docker.md")?.readContent();
-    return trie;
-}
+    beforeEach(() => {
+        trie = new Trie();
+    });
 
-const trie = getDefaultTrie();
-console.log(trie.roots.get('d')?.metadata.promisingMaterials[0]);
+    // Basic Case
+    test('Adding and finding single words', () => {
+        trie.addNode('apple');
+        expect(trie.findNode('apple')).not.toBeNull();
+        expect(trie.findNode('appl')).not.toBeNull();
+        expect(trie.findNode('apples')).toBeNull();
+    });
 
-describe('Trie material suggestion - count usage', () => {
-    test('docker on the first suggest', () => {
-        // This is wrong use of test. This is for sake of testing the testing.
-        expect(trie.roots.get('d')?.metadata.promisingMaterials[0]?.useCount).toBe(1);
-    })
+    // Empty String Case - Add
+    test('Adding empty string', () => {
+        expect(trie.findNode('')).toBeNull();
+        trie.addNode('');
+        expect(trie.findNode('')).toBeNull();
+    });
+
+    test('Searching in trie', () => {
+        trie.addNode('bat');
+        trie.addNode('ball');
+        expect(trie.findNode('bat')).not.toBeNull();
+        expect(trie.findNode('ba')).not.toBeNull();
+        expect(trie.findNode('ball')).not.toBeNull();
+    });
+
+    // Adding Duplicates
+    test('Adding duplicate strings - root nodes', () => {
+        trie.addNode('c');
+        trie.addNode('c');
+    });
+
+    test('Adding duplicate strings - non root nodes', () => {
+        trie.addNode('cat');
+        trie.addNode('cat');
+    });
+})
+
+describe('NodeMaterial and NodeMetadata', () => {
+
 });
