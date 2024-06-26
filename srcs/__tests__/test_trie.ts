215a25d7cdf5e95672093b97b93e61ab35303421
Concepty <khs1903b@gmail.com>
2024-06-26 15:27:23 +0900

    test: test for PrefixTree (red state)

---
+++ srcs/__tests__/test_trie.ts
@@ -0,0 +1,53 @@
+import { PrefixTree, Node } from "../trie";
+
+describe('PrefixTree basic operations', () => {
+    let trie: PrefixTree;
+    const test_set: string[] = [
+        'apple', 'apricot', 'apartment', 'apply', 'approach',
+        'banana', 'band', 'banner', 'bank', 'barrel',
+        'cat', 'catalog', 'catch', 'category', 'cater',
+        'dog', 'door', 'dorm', 'dot', 'double',
+        'elephant', 'elevator', 'eleven', 'elite', 'email',
+        'fish', 'fist', 'fiscal', 'fit', 'fiction',
+        'goat', 'goal', 'golf', 'gone', 'good',
+        'hat', 'hate', 'hatch', 'happy', 'harden',
+        'ice', 'icon', 'idle', 'ideal', 'ignite',
+        'jacket', 'jail', 'jam', 'jazz', 'jewel',
+        'kangaroo', 'kettle', 'kept', 'key', 'king',
+        'lamp', 'land', 'lane', 'large', 'laser',
+        'monkey', 'monitor', 'month', 'moody', 'motor',
+        'note', 'noble', 'node', 'noise', 'north',
+        'orange', 'orbit', 'organ', 'origin', 'ornament',
+        'panda', 'panic', 'panel', 'pant', 'park',
+        'queen', 'quick', 'quit', 'quilt', 'quote',
+        'rabbit', 'race', 'rack', 'radio', 'range',
+        'snake', 'snap', 'snow', 'soap', 'soft',
+        'tiger', 'tight', 'tire', 'title', 'toast',
+        'umbrella', 'unicorn', 'unit', 'unique', 'user',
+        'violet', 'vital', 'voice', 'void', 'volcano',
+        'water', 'waste', 'wave', 'wax', 'web',
+        'xylophone', 'xenon', 'xerox', 'x-ray', 'xenon',
+        'yak', 'yard', 'yarn', 'year', 'yellow',
+        'zebra', 'zero', 'zone', 'zoom', 'zulu'
+    ]
+    const test_set_not_included: string[] = [
+        'dapple', 'hatered', 'double monitor', 'july'
+    ]
+
+    beforeAll(() => {
+        trie = new PrefixTree();
+        for (const word of test_set) trie.add(word);
+    })
+
+    test('search exisitng nodes', () => {
+        for (const word of test_set) {
+            expect(trie.search(word)).not.toBeNull();
+        }
+    });
+
+    test('search non-existing nodes', () => {
+        for (const word of test_set_not_included) {
+            expect(trie.search(word)).toBeNull();
+        }
+    })
+});
