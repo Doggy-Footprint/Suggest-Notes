ceaa21c782b92ba8315404a4ee4569fea832da9e
Concepty <khs1903b@gmail.com>
2024-07-12 12:59:48 +0900

    add: (unstable) async message queue
    Synchronization safety is not tested or properly designed.
    Test and validation required

--- obsidian_srcs/main.ts
+++ obsidian_srcs/main.ts
@@ -1,12 +1,19 @@
-import { App, Plugin, TFile, MarkdownView, EditorSuggest, EditorSuggestTriggerInfo, EditorSuggestContext, EditorPosition, Editor } from 'obsidian';
+import { App, Plugin, TFile, MarkdownView, EditorSuggest, EditorSuggestTriggerInfo, EditorSuggestContext, EditorPosition, Editor, PluginManifest } from 'obsidian';
 import { PrefixTree, Node, Content, Keyword } from '../srcs/trie'
 // import { TFileContent } from './obsidian_trie'
 
 const TEST_KEYWORDS_DIRECTORY = 'TEST_KEYWORDS/KEYWORDS';
 const TEST_KEYWORDS_TAGS = ['keyword', 'pkm', 'frequently-used'];
 
-function getFileName(file: TFile): string {
-    return file.name.split('.')[0];
+function getFileName(file: TFile | string): string {
+    if (file instanceof TFile)
+        return file.name.split('.')[0];
+    else if (typeof file === 'string') {
+        const pathArray = file.split('/');
+        const name = pathArray[pathArray.length - 1];
+        return name.split('.')[0];
+    }
+    return ''; // shouldn't be used
 }
 
 function addFileinTrie(trie: PrefixTree<TFile>, file: TFile) {
@@ -14,7 +21,7 @@ function addFileinTrie(trie: PrefixTree<TFile>, file: TFile) {
 
     let content: Content<TFile> = new Content<TFile>(file);
     // TODO: edge-caes: abc.def.md
-    this.trie.add(getFileName(file), content); 
+    trie.add(getFileName(file), content); 
 
     const aliases = this.app.metadataCache.getFileCache(file)?.frontmatter?.aliases;
 
@@ -34,11 +41,133 @@ function isFileIcluded(file: TFile): boolean {
         return false;
 }
 
+interface EventMessage {
+    path: string,
+    file: TFile,
+    aliases: string[],
+    changes: 'new' | 'delete' | 'modify'
+}
+
+class MessageQueue {
+    app: App;
+    trie: PrefixTree<TFile>;
+    messages: EventMessage[];
+    head: number;
+    tail: number;
+    static readonly cutoff = 30;
+    static readonly maximum_size = 1000;
+
+    constructor(app: App, trie: PrefixTree<TFile>) {
+        this.app = app;
+        this.messages = [];
+        this.head = 0;
+        this.tail = 0;
+    }
+
+    // TODO check synchronization safety
+    public async enqueue(message: EventMessage) {
+        if (this.head === this.tail && this.head !== 0) {
+            this.resetQueue();
+        }
+        if (this.head > MessageQueue.cutoff) {
+            // discard finished messages
+            this.messages.splice(0, this.head);
+            this.head = 0;
+            this.tail = this.tail - MessageQueue.cutoff;
+        }
+        if (this.tail - this.head > MessageQueue.maximum_size)
+            console.error('MessageQueue oversized');
+        this.tail++;
+        this.messages[this.tail] = message;
+    }
+
+    // TODO check synchronization safety
+    public async processSingleMessage() {
+        if (this.head === this.tail) return;
+        
+        const message = this.messages[this.head++];
+
+        switch (message.changes) {
+            case 'new': { // currently not used
+                // TODO: implement bulk add in PrefixTree and use it.
+                const name = getFileName(message.path);
+                const content = new Content<TFile>(message.file)
+                this.trie.add(name, content);
+                message.aliases.forEach(alias => this.trie.add(alias, content));
+                break;
+            }
+            case 'delete': { // currently not used
+                const name = getFileName(message.path);
+                const content = this.trie.search(name)?.getContent(message.file);
+                if (!content) break;
+                content.cleanUp();
+                break;
+            }
+            case 'modify': {
+                // TODO: read aliases from file cache when processing 
+                const name = getFileName(message.path);
+                const content = this.trie.search(name)?.getContent(message.file);
+                const keywords = content?.getAllKeywords().map(k => k.keyword);
+
+                const aliases = this.app.metadataCache.getFileCache(message.file)?.frontmatter?.aliases;
+
+                if (!keywords || !aliases) break;
+                keywords.sort();
+                aliases.sort();
+
+                let i = 0, j = 0;
+                while (i < keywords.length && j < aliases.length) {
+                    if (i < keywords.length && keywords[i] === aliases[j]) {
+                        j++;
+                        i++;
+                        continue;
+                    } else if (keywords[i] > aliases[j]) {
+                        // missing in keywords - new aliases
+                        this.trie.add(aliases[j], content);
+                        j++;
+                    } else if (keywords[i] < aliases[j]) {
+                        // missing in aliases - deleted aliases
+                        this.trie.delete(message.file, keywords[i]);
+                        i++;
+                    }
+                }
+                break;
+            }
+            default: {
+                console.error('Unknown Changes type in EventMessage')
+            }
+        }
+
+        if (this.head === this.tail)
+            this.resetQueue();
+    }
+
+    public processAll() {
+        while (this.head !== this.tail) {
+            this.processSingleMessage();
+        }
+        this.resetQueue();
+    }
+
+    private resetQueue() {
+        this.messages = [];
+        this.head = 0;
+        this.tail = 0;
+    }
+}
+
 export default class KeywordSuggestPlugin extends Plugin {
-    trie: PrefixTree<TFile> = new PrefixTree<TFile>();
+    trie: PrefixTree<TFile>;
+    messageQueue: MessageQueue;
+
+    constructor(app: App, manifest: PluginManifest) {
+        super(app, manifest);
+        this.trie = new PrefixTree<TFile>();
+        this.messageQueue = new MessageQueue(this.app, this.trie);
+    }
 
     onload() {
-        this.registerEditorSuggest(new LinkSuggest(this.app));
+        this.registerEditorSuggest(new LinkSuggest(this.app, this.trie, this.messageQueue));
 
         this.registerEventListeners();
 
@@ -49,22 +178,30 @@ export default class KeywordSuggestPlugin extends Plugin {
         // create
         this.app.workspace.onLayoutReady(() => {
             this.registerEvent(this.app.vault.on('create', file => {
-                if (file instanceof TFile && isFileIcluded(file)) {
-                    addFileinTrie(this.trie, file);
-                }
+                if (!(file instanceof TFile) || !isFileIcluded(file)) return;
+                console.log('on create')
+                addFileinTrie(this.trie, file);
             }));
         });
 
         // modify
-        // TODO
-        // use Event queue, read from queue when suggest to user
         this.registerEvent(this.app.vault.on('modify', file => {
-
+            if (!(file instanceof TFile) || !isFileIcluded(file)) return;
+            console.log('on modify');
+            this.messageQueue.processSingleMessage();
+            this.messageQueue.enqueue({
+                path: file.path,
+                file: file,
+                aliases: [],
+                changes: 'modify'
+            })
         }));
 
         //  delete
         this.registerEvent(this.app.vault.on('delete', file => {
             if (!(file instanceof TFile)) return;
+
+            console.log('on delete');
 
             const node = this.trie.search(getFileName(file));
             const content = node?.getContent(file)?.cleanUp();
@@ -73,6 +210,8 @@ export default class KeywordSuggestPlugin extends Plugin {
         // TODO
         this.registerEvent(this.app.vault.on('rename', (file, oldPath) => {
             if (!(file instanceof TFile)) return;
+
+            console.log('on rename');
 
             const oldPathArray = oldPath.split('/');
             const name = oldPathArray[oldPathArray.length - 1];
@@ -94,10 +233,13 @@ interface TFileContent {
 }
 
 export class LinkSuggest extends EditorSuggest<TFileContent> {
-    trie: PrefixTree<TFile> = new PrefixTree<TFile>();
+    trie: PrefixTree<TFile>;
+    messageQueue: MessageQueue;
 
-    constructor(app: App) {
+    constructor(app: App, trie: PrefixTree<TFile>, messageQueue: MessageQueue) {
         super(app);
+        this.trie = trie;
+        this.messageQueue = messageQueue;
         this.loadFiles();
         this.limit = 8; // TODO: ask someone with UI/UX knowledge
     }
@@ -110,6 +252,7 @@ export class LinkSuggest extends EditorSuggest<TFileContent> {
     }
 
     public onTrigger(cursor: EditorPosition, editor: Editor, file: TFile | null): EditorSuggestTriggerInfo | null {
+        this.messageQueue.processAll(); // TODO check if is this right place to processAll
         if (cursor.ch == 0) return null;
 
         const { word, startIndex } = this.getWord(cursor, editor);
@@ -176,6 +319,9 @@ export class LinkSuggest extends EditorSuggest<TFileContent> {
         if (!this.context) return;
         const { start, end } = this.context;
         value.content.readWithKeyword(value.keyword.keyword);
+        // score is for testing
         this.context?.editor.replaceRange(`[[${value.content.read().name.split('.')[0]}|${value.keyword.keyword} - ${value.keyword.getScore()}]]`, start, end);
+
+        // Issue: if alias is the same with name of note or other aliases ignoring case, errorneous behavior happens
     }
 }
