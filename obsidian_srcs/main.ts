dc431014389979e002d33530421a58a50808f763
Concepty <khs1903b@gmail.com>
2024-07-17 10:13:25 +0900

    update: main
    Changes:
        - remove MessageQueue system
        - use CachedMetadata events
    
    Add:
        - measurePerformance
    
    Update:
        - isFileIcluded use cache if provided

--- obsidian_srcs/main.ts
+++ obsidian_srcs/main.ts
@@ -1,165 +1,29 @@
-import { App, Plugin, TFile, MarkdownView, EditorSuggest, EditorSuggestTriggerInfo, EditorSuggestContext, EditorPosition, Editor, PluginManifest } from 'obsidian';
+import { App, Plugin, TFile, MarkdownView, EditorSuggest, EditorSuggestTriggerInfo, EditorSuggestContext, EditorPosition, Editor, PluginManifest, CachedMetadata } from 'obsidian';
 import { PrefixTree, Content, Keyword } from '../srcs/trie'
 import { DEFAULT_SETTINGS, KeywordSuggestPluginSettings, KeywordSuggestPluginSettingTab } from './settings'
 
 const isDev = process.env.NODE_ENV === 'development';
 
-interface EventMessage {
-    path: string,
-    file: TFile,
-    aliases?: string[],
-    changes: 'new' | 'delete' | 'modify'
-}
+function measurePerformance<T>(lines: () => T, label: string, desc?: string): T {
+    if (!isDev) return lines();
+    
+    performance.mark(`start - ${label}`);
 
-class MessageQueue {
-    app: App;
-    plugin: KeywordSuggestPlugin
-    trie: PrefixTree<TFile>;
-    messages: EventMessage[];
-    head: number;
-    tail: number;
-    static readonly cutoff = 30;
-    static readonly maximum_size = 1000;
+    const result = lines();
 
-    constructor(app: App, plugin: KeywordSuggestPlugin, trie: PrefixTree<TFile>) {
-        this.app = app;
-        this.plugin = plugin
-        this.trie = trie;
-        this.messages = [];
-        this.head = 0;
-        this.tail = 0;
-    }
-
-    public enqueue(message: EventMessage) {
-        if (this.head === this.tail && this.head !== 0) {
-            this.resetQueue();
-        }
-        if (this.head > MessageQueue.cutoff) {
-            // discard finished messages
-            this.messages.splice(0, this.head);
-            this.head = 0;
-            this.tail = this.tail - MessageQueue.cutoff;
-        }
-        if (this.tail - this.head > MessageQueue.maximum_size)
-            console.error('MessageQueue oversized');
-        this.messages[this.tail++] = message;
-    }
-
-    public processSingleMessage() {
-        if (isDev)
-            performance.mark('start - processSingleMessage');
-
-        if (this.head === this.tail) return;
-        
-        const message = this.messages[this.head++];
-        const name = this.plugin.getFileName(message.path);
-
-        switch (message.changes) {
-            case 'new': {
-                const content = new Content<TFile>(message.file);
-                this.trie.add(name, content);
-                
-                const aliases = this.app.metadataCache.getFileCache(message.file)?.frontmatter?.aliases;
-                if (!aliases) break;
-                
-                aliases.forEach((alias: string) => this.trie.add(alias, content));
-                break;
-            }
-            case 'delete': { // currently not used
-                const content = this.trie.search(name)?.getContent(message.file);
-                if (content) content.cleanUp();
-                break;
-            }
-            case 'modify': {
-                const content = this.trie.search(name)?.getContent(message.file);
-                const keywords = content?.getAllKeywords().map(k => k.keyword);
-
-                const aliases = this.app.metadataCache.getFileCache(message.file)?.frontmatter?.aliases;
-
-                if (!keywords || !aliases) break;
-
-                aliases.push(name);
-
-                keywords.sort();
-                aliases.sort();
-
-                let i = 0, j = 0;
-                while (i < keywords.length && j < aliases.length) {
-                    if (i < keywords.length && keywords[i] === aliases[j]) {
-                        j++;
-                        i++;
-                        continue;
-                    } else if (keywords[i] > aliases[j]) {
-                        // missing in keywords - new aliases
-                        this.trie.add(aliases[j++], content);
-                    } else if (keywords[i] < aliases[j]) {
-                        // missing in aliases - deleted aliases
-                        this.trie.delete(keywords[i++], message.file);
-                    }
-                }
-
-                while (j < aliases.length) {
-                    this.trie.add(aliases[j++], content);
-                }
-                
-                while (i < keywords.length) {
-                    this.trie.delete(keywords[i++], message.file);
-                }
-                
-                break;
-            }
-            default: {
-                console.error('Unknown Changes type in EventMessage')
-            }
-        }
-
-        if (this.head === this.tail)
-            this.resetQueue();
-
-        if (isDev) {
-            performance.mark('end - processSingleMessage');
-            performance.measure('processSingleMessage', 'start - processSingleMessage', 'end - processSingleMessage');
-
-            const measure = performance.getEntriesByName('processSingleMessage')[0];
-            console.log(`\t\tprocessSingleMessage with ${message.changes} took ${measure.duration} ms`);
-        }
-    }
-
-    public processAll() {
-        if (isDev) {
-            performance.mark('start - processAll');
-        }
-
-        while (this.head !== this.tail) {
-            this.processSingleMessage();
-        }
-        this.resetQueue();
-
-        if (isDev) {
-            performance.mark('end - processAll');
-            performance.measure('processAll', 'start - processAll', 'end - processAll');
-
-            const measure = performance.getEntriesByName('processAll')[0];
-            console.log(`\tprocessAll took ${measure.duration} ms`);
-        }
-    }
-
-    private resetQueue() {
-        this.messages = [];
-        this.head = 0;
-        this.tail = 0;
-    }
+    performance.mark(`end - ${label}`);
+    performance.measure(label, `start - ${label}`, `end - ${label}`);
+    const measure = performance.getEntriesByName(label)[0];
+    console.log(`${label} ${desc ?? ''} took ${measure.duration} ms`);
 }
 
 export default class KeywordSuggestPlugin extends Plugin {
     trie: PrefixTree<TFile>;
-    messageQueue: MessageQueue;
     settings: KeywordSuggestPluginSettings
 
     constructor(app: App, manifest: PluginManifest) {
         super(app, manifest);
         this.trie = new PrefixTree<TFile>();
-        this.messageQueue = new MessageQueue(this.app, this, this.trie);
     }
 
     async onload() {
@@ -169,7 +33,7 @@ export default class KeywordSuggestPlugin extends Plugin {
         // TODO : after saving usage, load the saved usage to set each Content and Keyword object
         this.app.vault.getFiles().forEach(file => this.addFileinTrie(this.trie, file));
 
-        this.registerEditorSuggest(new LinkSuggest(this.app, this.trie, this.messageQueue));
+        this.registerEditorSuggest(new LinkSuggest(this.app, this.trie));
         this.registerEventListeners();
     }
 
@@ -179,51 +43,23 @@ export default class KeywordSuggestPlugin extends Plugin {
         // Postpone useCount to dev later
     }
 
-    private registerEventListeners() {
-        this.app.workspace.onLayoutReady(() => {
-            this.registerEvent(this.app.vault.on('create', file => {
-                if (!(file instanceof TFile) || !this.isFileIcluded(file)) return;
-                console.log('on create')
-
-                this.messageQueue.processSingleMessage();
-                this.messageQueue.enqueue({
-                    path: file.path,
-                    file: file,
-                    changes: 'new'
-                })
-            }));
-        });
-
-        this.registerEvent(this.app.vault.on('modify', file => {
+    private registerEventListeners() {        
+        this.registerEvent(this.app.metadataCache.on('changed', (file, _, cache) => {
             if (!(file instanceof TFile) || !this.isFileIcluded(file)) return;
-            console.log('on modify');
-            this.messageQueue.processSingleMessage();
-            this.messageQueue.enqueue({
-                path: file.path,
-                file: file,
-                changes: 'modify'
-            })
+            const aliases = cache.frontmatter?.aliases;
+
+            // TODO: how to distinguish create and modify? maybe use MessageQueue and both vault.create, and metadataCache.changed
         }));
-
-        this.registerEvent(this.app.vault.on('delete', file => {
-            if (!(file instanceof TFile) || !this.isFileIcluded(file)) return;
-
-            console.log('on delete');
-
-            const node = this.trie.search(this.getFileName(file));
-            const content = node?.getContent(file)?.cleanUp();
-        }));
-
+        
         this.registerEvent(this.app.vault.on('rename', (file, oldPath) => {
             if (!(file instanceof TFile) || !this.isFileIcluded(file)) return;
-
-            console.log('on rename');
-
-            const oldPathArray = oldPath.split('/');
-            const name = oldPathArray[oldPathArray.length - 1];
-
-            this.trie.move(name.split('.')[0], this.getFileName(file), file);
+            this.trie.move(this.getFileName(oldPath), this.getFileName(file), file);
         }));
+
+        this.registerEvent(this.app.metadataCache.on('deleted', (file, prevCache) => {
+            if (!(file instanceof TFile) || !this.isFileIcluded(file)) return;
+            // TODO
+        }))
     }
 
     async onunload() {
@@ -259,13 +95,16 @@ export default class KeywordSuggestPlugin extends Plugin {
         }
     }
 
-    isFileIcluded(file: TFile): boolean {
-    if (this.settings.searchDirectories.some(dir => file.path.startsWith(dir)))
-        return true;
-    if (this.app.metadataCache.getFileCache(file)?.frontmatter?.tags?.some((t: string) => this.settings.checkTags.includes(t)))
-        return true;
-    return false;
-}
+    isFileIcluded(file: TFile, cache: CachedMetadata | null = null): boolean {
+        if (!cache) cache = this.app.metadataCache.getFileCache(file);
+
+        const result = measurePerformance<boolean>(() => {
+            return this.settings.searchDirectories.some(dir => file.path.startsWith(dir))
+            || cache?.frontmatter?.tags?.some((t: string) => this.settings.checkTags.includes(t));
+        }, 'isFileIcluded');
+
+        return result;
+    }
 }
 
 interface TFileContent {
@@ -275,37 +114,22 @@ interface TFileContent {
 
 export class LinkSuggest extends EditorSuggest<TFileContent> {
     trie: PrefixTree<TFile>;
-    messageQueue: MessageQueue;
 
-    constructor(app: App, trie: PrefixTree<TFile>, messageQueue: MessageQueue) {
+    constructor(app: App, trie: PrefixTree<TFile>) {
         super(app);
         this.trie = trie;
-        this.messageQueue = messageQueue;
         this.limit = 8; // TODO: ask someone with UI/UX knowledge
     }
 
     public onTrigger(cursor: EditorPosition, editor: Editor, file: TFile | null): EditorSuggestTriggerInfo | null {
-        /**
-         * NOTE
-         * onTrigger is required to quickly reply. But as the this.messageQueue process messages on each modify, I assumed
-         * that the number of remaining messages would be relatively ignorably small.
-         */
-        this.messageQueue.processAll();
         if (cursor.ch == 0) return null;
 
         const { word, startIndex } = this.getWord(cursor, editor);
         if (word.length < 2) return null;
 
-        if (isDev) {
-            performance.mark('start - getSuggestion in onTrigger');
-        }
-        const suggestions = this.trie.search(word)?.getSuggestion();
-        if (isDev) {
-            performance.mark('end - getSuggestion in onTrigger');
-            performance.measure('getSuggestion in onTrigger', 'start - getSuggestion in onTrigger', 'end - getSuggestion in onTrigger');
-            const measure = performance.getEntriesByName('getSuggestion in onTrigger')[0];
-            console.log(`getSuggestion in onTrigger took ${measure.duration} ms`);
-        }
+        const suggestions = measurePerformance<Content<TFile>[] | undefined>(() => {
+            return this.trie.search(word)?.getSuggestion();
+        }, 'getSuggestion', 'onTrigger')
 
         if (!suggestions) return null;
 
